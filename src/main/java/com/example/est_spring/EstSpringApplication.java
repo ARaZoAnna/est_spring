d145cb0647643f690d7e1ac0599462cadce91755
zoanna <zoanna5442@gmail.com>
2024-07-09 14:46:34 +0900

    Ormi5기 Spring 7/9 4일차

--- src/main/java/com/example/est_spring/EstSpringApplication.java
+++ src/main/java/com/example/est_spring/EstSpringApplication.java
@@ -2,20 +2,34 @@ package com.example.est_spring;
 
 
 import com.example.day0709.Message;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.servlet.DispatcherServlet;
+
+import java.util.TimeZone;
 
 
 @SpringBootApplication(
         scanBasePackages = "com.example.day0709")
 public class EstSpringApplication {
 
-    @Autowired
-    @Qualifier("sendMessage")
-    Message message;
+    @Value("${server.port}")
+    private int port;
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @PostConstruct
+    public void printConfig() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+        System.out.println("포트번호: " + port);
+        System.out.println("애플리케이션 이름: " + appName);
+    }
     public static void main(String[] args) {
 
         SpringApplication.run(EstSpringApplication.class, args);
